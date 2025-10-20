792833a46a8b1e9cc99f58a7926f07f54f510056
JorgeCastro13ppp <jlcastroc01@educarex.es>
2025-10-20 11:50:25 +0200

    Hacer todos los ejercicios

--- src/ejercicios/AlumnoEj29.java
+++ src/ejercicios/AlumnoEj29.java
@@ -1,5 +1,75 @@
 package ejercicios;
 
-public class AlumnoEj29 implements Serializable{ //Im
+import java.io.Serializable;
+import java.util.Objects;
 
+public class AlumnoEj29 implements Serializable{ 
+	private static final long SerialVersionUID = 1L; //Recomendado para objetos serializables
+	
+	private int expediente;
+	private String nombre;
+	private double nota;
+	
+	public AlumnoEj29() {
+		
+	}
+
+	public AlumnoEj29(int expediente, String nombre, double nota) {
+		super();
+		this.expediente = expediente;
+		this.nombre = nombre;
+		//this.nota = nota;
+		this.nota = -1; //Modificamos la nota al inicializar el constructor para saber que no tiene nota 
+	}
+
+	public int getExpediente() {
+		return expediente;
+	}
+
+	public void setExpediente(int expediente) {
+		this.expediente = expediente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getNota() {
+		return nota;
+	}
+
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+
+	@Override
+	public String toString() {
+		String notaTexto = (nota == -1) ? "Sin nota" : String.format("%.2f", nota);
+		return "AlumnoEj29 [expediente=" + expediente + ", nombre=" + nombre + ", nota=" + nota + "]";
+	}
+	
+	//Relizamos el hashCode y el equals basados en el expediente porque solo hay uno por alumno
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(expediente);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AlumnoEj29 other = (AlumnoEj29) obj;
+		return expediente == other.expediente;
+	}
+	
+	
 }
--- src/ejercicios/Ejercicio29.java
+++ src/ejercicios/Ejercicio29.java
@@ -1,10 +1,111 @@
 package ejercicios;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Ejercicio29 {
+	
+	private static final String FICHERO = "alumnos.bin";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		Set<AlumnoEj29> alumnos = leerFichero();
+		int opcion;
+		
+		do {
+			System.out.println("\n--- GESTIÓN DE ALUMNOS ---");
+            System.out.println("1. Añadir alumno");
+            System.out.println("2. Mostrar alumnos");
+            System.out.println("3. Poner nota");
+            System.out.println("4. Mostrar estadística");
+            System.out.println("5. Borrar alumno");
+            System.out.println("6. Salir");
+            System.out.print("Opción: ");
+            
+            opcion = sc.nextInt();
+            sc.nextLine();
+            
+            switch(opcion) {
+            case 1:
+            	break;
+            case 2:
+            	break;
+            case 3:
+            	break;
+            case 4:
+            	break;
+            case 5:
+            	break;
+            case 6:
+            	System.out.println("Saliendo...");
+            	break;
+            default:
+            	System.out.println("Opción no válida.");
+            	break;
+            }
+            
+            guardarFichero(alumnos);
+			
+		}while(opcion!=6);
+		
+		sc.close();
 
 	}
+	
+	private static Set<AlumnoEj29> leerFichero(){
+		File ficheroLeer = new File(FICHERO);
+		Set<AlumnoEj29> alumnosLeer= new LinkedHashSet<>(); 
+		
+		if(!ficheroLeer.exists()) {
+			return alumnosLeer;
+		}
+		
+		try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ficheroLeer))){
+			alumnosLeer = (Set <AlumnoEj29>)  ois.readObject();
+		}catch(IOException | ClassNotFoundException e) {
+			System.out.println("Error al leer el fichero: "+e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return alumnosLeer;
+	}
+	
+	private static void guardarFichero(Set<AlumnoEj29>alumnos) {
+		
+		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FICHERO))) {
+			oos.writeObject(alumnos);
+		}catch(IOException e) {
+			System.out.println("Error al guardar el fichero: "+e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	private static void anadirAlumno(Scanner sc, Set<AlumnoEj29>alumnos) {
+		System.out.println("Introduce el número del expediente a añadir:");
+		int expedienteAnadir= sc.nextInt();
+		sc.nextLine();
+		
+		for(AlumnoEj29 a: alumnos) {
+			if(a.getExpediente()==expedienteAnadir) {
+				System.out.println("Ya existe un alumno con ese expediente.");
+				return;
+			}
+		}
+		
+		System.out.println("Introduce el nombre del alumno a añadir:");
+		String nombreAlumnoAnadir = sc.nextLine();
+		
+		System.out.println("Introduce nota a añadir:");
+		
+	}
 
+	
 }
